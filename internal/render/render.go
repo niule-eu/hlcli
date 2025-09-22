f49056eaa5f7903189cc092ad2be2f112628e359
niule <to.nils.hansen@gmail.com>
2025-09-22 14:30:03 +0000

    Makes render_pkl behave mor like pkl eval
    pkl eval will search for a PklProject file along the path of given module,
    this change implements that behavior for render_pkl subcommand.

--- internal/render/render.go
+++ internal/render/render.go
@@ -2,10 +2,13 @@ package render
 
 import (
 	"context"
+	"fmt"
+	"io/fs"
+	"log"
 	"net/url"
 
 	// "log"
-	"path"
+
 	"path/filepath"
 	"strings"
 
@@ -21,6 +24,27 @@ import (
 var (
 	parser = yaml.Parser()
 )
+
+type PklProjectNotFoundError struct {
+	Module string
+}
+
+func (e *PklProjectNotFoundError) Error() string {
+	return fmt.Sprintf("No PklProject found for module '%s'", e.Module)
+}
+
+type MultiplePklProjectFoundError struct {
+	Module          string
+	PklProjectFiles []string
+}
+
+func (e *MultiplePklProjectFoundError) Error() string {
+	return fmt.Sprintf(
+		"More than 1 PklProject found for module '%s':\n%s",
+		e.Module,
+		strings.Join(e.PklProjectFiles, "\n"),
+	)
+}
 
 type SopsResourceReader struct {
 	secrets *koanf.Koanf
@@ -50,32 +74,31 @@ type RenderPklParams struct {
 	PklFile        string
 	OutputFile     string
 	AllowedModules []string
+	PklProjectFile string
 }
 
 func RenderPkl(params RenderPklParams, secrets *koanf.Koanf) (framework.Effect, error) {
 	var err error
-	oldwd, err := os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-	newwd, err := filepath.Abs(path.Dir(params.PklFile))
-	if err != nil {
-		return nil, err
-	}
-	err = os.Chdir(newwd)
+
+	pkl_project_root, err := findPklProjectRoot(params.PklFile, params.PklProjectFile)
 	if err != nil {
 		return nil, err
 	}
 
-	evaluator, err := pkl.NewEvaluator(context.Background(), pkl.PreconfiguredOptions, func(options *pkl.EvaluatorOptions) {
-		options.ResourceReaders = append(options.ResourceReaders, SopsResourceReader{secrets: secrets})
-		options.AllowedResources = append(options.AllowedResources, "sops")
-	})
+	evaluatorManager := pkl.NewEvaluatorManager()
+	evaluator, err := evaluatorManager.NewProjectEvaluator(
+		context.Background(),
+		pkl_project_root,
+		pkl.PreconfiguredOptions,
+		func(options *pkl.EvaluatorOptions) {
+			options.ResourceReaders = append(options.ResourceReaders, SopsResourceReader{secrets: secrets})
+			options.AllowedResources = append(options.AllowedResources, "sops")
+		},
+	)
 	if err != nil {
 		return nil, err
 	}
 	defer evaluator.Close()
-	os.Chdir(oldwd)
 
 	data, err := evaluator.EvaluateOutputText(context.Background(), pkl.FileSource(params.PklFile))
 	if err != nil {
@@ -83,4 +106,42 @@ func RenderPkl(params RenderPklParams, secrets *koanf.Koanf) (framework.Effect, 
 	}
 
 	return framework.NewDefaultFileWriteIO(params.OutputFile, []byte(data)), nil
+}
+
+func findPklProjectRoot(mod_path string, project_path string) (string, error) {
+	// If project_path provided, check if it exists and if it does, return it
+	if project_path != "" {
+		if _, err := os.Stat(project_path); err == nil {
+			return project_path, nil
+		}
+	}
+
+	// project_path does not exist or not povided, search for it along mod_path
+	wd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	pkl_project_file_paths := []string{}
+	filepath.WalkDir(wd, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			if !strings.HasPrefix(mod_path, path) {
+				return filepath.SkipDir
+			}
+		} else if d.Name() == "PklProject" {
+			pkl_project_file_paths = append(pkl_project_file_paths, filepath.Dir(path))
+		}
+		return nil
+	})
+	if len(pkl_project_file_paths) == 1 {
+		return pkl_project_file_paths[0], nil
+	} else if len(pkl_project_file_paths) == 0 {
+		return "", &PklProjectNotFoundError{
+			Module: mod_path,
+		}
+	} else {
+		return "", &MultiplePklProjectFoundError{
+			Module:          mod_path,
+			PklProjectFiles: pkl_project_file_paths,
+		}
+	}
 }
